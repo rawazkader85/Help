d732d42f074e5e74248fc401944d3ae7ecb6e628
rawazkader85 <rkade103@uottawa.ca>
2016-12-01 17:16:22 -0500

    This is an updated version of the help thing

--- app/src/main/java/ca/uottawa/help/helpActivity.java
+++ app/src/main/java/ca/uottawa/help/helpActivity.java
@@ -4,47 +4,70 @@ import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.EditText;
+import android.widget.ScrollView;
+import android.widget.TextView;
+
+import static ca.uottawa.help.R.id.scrollView;
 
 public class helpActivity extends AppCompatActivity {
+    private TextView introductionText;
+    private TextView signUpText;
+    private TextView loginScreenHelp;
+    private TextView searchHelp;
+    private TextView favouriteHelp;
+    private TextView recipeMakingHelp;
+    private ScrollView helpScroll;
 
-    public void signUpJump(View view){
-        EditText eText = (EditText)findViewById(R.id.layoutOfHelp);
-        eText.setText(eText.getText()+"•Signing up to a Recipeek account.");
+    public void onClickFocusText(View view){
+        int pressId = view.getId();
+        switch(pressId) {
+            case R.id.signUpJump:
+                helpScroll.smoothScrollTo(0,signUpText.getTop());
+                break;
+            case R.id.loggingInJump:
+                helpScroll.smoothScrollTo(0,loginScreenHelp.getTop());
+                break;
+            case R.id.searchJump:
+                helpScroll.smoothScrollTo(0,searchHelp.getTop());
+                break;
+            case R.id.favouriteJump:
+                helpScroll.smoothScrollTo(0,favouriteHelp.getTop());
+                break;
+            case R.id.createRecipeJump:
+                helpScroll.smoothScrollTo(0,recipeMakingHelp.getTop());
+                break;
+            case R.id.topButton1:
+                helpScroll.smoothScrollTo(0,introductionText.getTop());
+                break;
+            case R.id.topButton2:
+                helpScroll.smoothScrollTo(0,introductionText.getTop());
+                break;
+            case R.id.topButton3:
+                helpScroll.smoothScrollTo(0,introductionText.getTop());
+                break;
+            case R.id.topButton4:
+                helpScroll.smoothScrollTo(0,introductionText.getTop());
+                break;
+            case R.id.topButton5:
+                helpScroll.smoothScrollTo(0,introductionText.getTop());
+                break;
+            default:
+                helpScroll.smoothScrollTo(0,introductionText.getTop());
+                break;
+        }
     }
-
-    public void loggingInJump(View view){
-        EditText eText = (EditText)findViewById(R.id.layoutOfHelp);
-        eText.setText(eText.getText()+"•Logging into your Recipeek account.");
-    }
-
-    public void searchJump(View view){
-        EditText eText = (EditText)findViewById(R.id.layoutOfHelp);
-        eText.setText(eText.getText()+"•Searching for a recipe.");
-    }
-
-    public void favourtieJump(View view){
-        EditText eText = (EditText)findViewById(R.id.layoutOfHelp);
-        eText.setText(eText.getText()+"•Favouriting a recipe.");
-    }
-
-    public void createRecipeJump(View view){
-        EditText eText = (EditText)findViewById(R.id.layoutOfHelp);
-        eText.setText(eText.getText()+"•Making a recipe and uploading it online.");
-    }
-
-
-
-    protected void focusOnSignUpHelp(){
-
-    }
-
-
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_help);
         ActionBar ab = getSupportActionBar();
         ab.setTitle("Help");
+        introductionText = (TextView) findViewById(R.id.introductionText);
+        signUpText = (TextView) findViewById(R.id.signUpHelp);
+        loginScreenHelp = (TextView) findViewById(R.id.loginScreenHelp);
+        searchHelp = (TextView) findViewById(R.id.searchHelp);
+        favouriteHelp = (TextView) findViewById(R.id.favouriteHelp);
+        recipeMakingHelp = (TextView) findViewById(R.id.recipeMakingHelp);
+        helpScroll = (ScrollView) findViewById(R.id.helpScroll);
     }
 }
