42e08947ea3988c4ab34557cad57da19de334e35
rawazkader85 <rkade103@uottawa.ca>
2016-11-30 22:26:56 -0500

    This is an updated version of the help thing

--- app/src/main/java/ca/uottawa/help/helpActivity.java
+++ app/src/main/java/ca/uottawa/help/helpActivity.java
@@ -1,19 +1,50 @@
 package ca.uottawa.help;
 
-import android.graphics.Color;
-import android.graphics.drawable.ColorDrawable;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class helpActivity extends AppCompatActivity {
 
-    @Override
+    public void signUpJump(View view){
+        EditText eText = (EditText)findViewById(R.id.layoutOfHelp);
+        eText.setText(eText.getText()+"•Signing up to a Recipeek account.");
+    }
+
+    public void loggingInJump(View view){
+        EditText eText = (EditText)findViewById(R.id.layoutOfHelp);
+        eText.setText(eText.getText()+"•Logging into your Recipeek account.");
+    }
+
+    public void searchJump(View view){
+        EditText eText = (EditText)findViewById(R.id.layoutOfHelp);
+        eText.setText(eText.getText()+"•Searching for a recipe.");
+    }
+
+    public void favourtieJump(View view){
+        EditText eText = (EditText)findViewById(R.id.layoutOfHelp);
+        eText.setText(eText.getText()+"•Favouriting a recipe.");
+    }
+
+    public void createRecipeJump(View view){
+        EditText eText = (EditText)findViewById(R.id.layoutOfHelp);
+        eText.setText(eText.getText()+"•Making a recipe and uploading it online.");
+    }
+
+
+
+    protected void focusOnSignUpHelp(){
+
+    }
+
+
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_help);
         ActionBar ab = getSupportActionBar();
         ab.setTitle("Help");
-        ab.setBackgroundDrawable(new ColorDrawable(Color.parseColor("#006400")));
     }
 }
